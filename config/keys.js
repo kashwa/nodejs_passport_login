08fa54492d4ed018930642efa6478f4cbf026590
kashwa <kashwa80@gmail.com>
2019-12-21 13:40:57 +0200

    Set keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://<USERNAME_GOES_HERE>:<PASSWORd_GOES_HERE>@mongodb-cluster-ylpa7.mongodb.net/test?retryWrites=true&w=majority',
-}
+  MongoURI:
+    "mongodb+srv://aabed123456:aabed123456@mongodb-cluster-ylpa7.mongodb.net/test?retryWrites=true&w=majority"
+};
